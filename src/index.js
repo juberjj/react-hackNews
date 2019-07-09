4151150a55acb5e1a0d13b694995932ef125c3e2
juberjj <juberjj@yahoo.ie>
2019-07-09 14:39:35 +0100

    Added authentication and related tests

--- src/index.js
+++ src/index.js
@@ -1,31 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import CommentPage from "./components/commentPage";
+import Header from "./components/header/";
+import LoginForm from "./components/authentication/loginForm";
 
-const Router = (props) => {
-  return (
+
+const Router = () => 
     <BrowserRouter>
       <div className="jumbotron">
+        <Header />
         <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-6 offset-3">
-              <h1>
-                <Link to="/">Hacker News</Link>
-              </h1>
-            </div>
-          </div>
+          <Switch>
+            <Route path="/posts/:post_id" component={CommentPage} />
+            <Route path="/login" component={LoginForm} />  {/* New route */}
+            <Route exact path="/" component={App} />
+            <Redirect from="*" to="/" />
+          </Switch>
         </div>
-        <Switch>
-          <Route path="/posts/:post_id" component={CommentPage} />
-          <Route exact path="/" component={App} />
-          <Redirect from="*" to="/" />
-        </Switch>
       </div>
     </BrowserRouter>
-  );
-};
 
 ReactDOM.render(<Router />, document.getElementById("root"));
